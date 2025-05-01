91daa14945f21c6ab28229fba73be376655f6e3a
23047fs <23047fs@hvhs.school.nz>
2025-05-01 05:41:34 +0000

    May 1, 2025, 5:41 AM

--- car_rentals.js
+++ car_rentals.js
@@ -14,11 +14,9 @@ let userNAME
 /********************************
 Main code
 ********************************/
-function start() {
+
 userNAME = propmt("Hello, what is your name")
 alert("Hello " + userNAME + " at Rob's Rentals we hope you find the right car for your needs that is why we provide up to an 8 seater")
-}
-
 
 /********************************
 Functions
