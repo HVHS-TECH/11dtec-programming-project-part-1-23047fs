6dcb322e77f49e0e4f0e4442c764c96d9081d4a4
23047fs <23047fs@hvhs.school.nz>
2025-05-05 21:12:35 +0000

    May 5, 2025, 9:12 PM

--- car_rentals.js
+++ car_rentals.js
@@ -1,7 +1,7 @@
 /********************************
 Name of task: Rental Car 
 Author: Finley
-Date: 01/05/35
+Start Date: 01/05/35
 ********************************/
 console.log("Running car_rentals.js");
 
@@ -22,41 +22,59 @@ let carSEATS = [1, 2, "null", 4, "null", 5, 7, 8];
 Main code
 ********************************/
 
-userNAME = prompt("Hello, what is your name");
-userAGE = prompt("Hello " + userNAME + ". What is your age");
-//Asks user name and age
-userAGE = Number(userAGE);
-//Converts string to number
-if (userAGE == 16) {
-    userLICENCE = prompt("Please verify if you have a licence, true or false");
-} else if (userAGE > 16) {
-    userLICENCE = true;
-} else if (userAGE <= 15) {
-    alert("Sorry but you can't have a car");
-    userLICENCE = false;
-}
-//Checks if user has a licence
-userCARseats = prompt("What would be the ideal number of seats in your vehicle " + userNAME)
-userCARseats = userCARseats - 1;
-if (userCARseats == 2) {
-    userCARseats = 3;
-}
-if (userCARseats == 5) {
-    userCARseats = 6;
-}
-//If req seats are 3 or 6 they round up one
-userRECOMMENDEDcar = cars[userCARseats]
-//Finds recommended car
-alert("The recommended car that meets your requirements is " + cars[userCARseats] + ", it has " + carSEATS[userCARseats] + " seats");
-//Tells user recommended car and its number of seats
+userWELCOME()
+userLICENCEcheck()
+findUSERcar()
+tellUSERrecommendedCAR()
 
-
-console.log(userNAME);
-console.log(userAGE);
-console.log(userLICENCE);
-console.log(userCARseats);
-console.log(userRECOMMENDEDcar);
-//Easy to check if info is right
+checkVARIABLES()
 /********************************
 Functions
 ********************************/
+function userWELCOME() {
+    userNAME = prompt("Hello, what is your name");
+    userAGE = prompt("Hello " + userNAME + ". What is your age");
+//Asks user name and age
+    userAGE = Number(userAGE);
+//Converts string to number
+}
+
+function userLICENCEcheck() {
+    if (userAGE == 16) {
+        userLICENCE = prompt("Please verify if you have a licence, true or false");
+    } else if (userAGE > 16) {
+        userLICENCE = true;
+    } else if (userAGE <= 15) {
+        alert("Sorry but you can't have a car");
+        userLICENCE = false;
+    }
+//Checks if user has a licence
+}
+
+function findUSERcar() {
+    userCARseats = prompt("What would be the ideal number of seats in your vehicle " + userNAME)
+    userCARseats = userCARseats - 1;
+    if (userCARseats == 2) {
+    userCARseats = 3;
+    }
+    if (userCARseats == 5) {
+    userCARseats = 6;
+    }
+//If req seats are 3 or 6 they round up one
+    userRECOMMENDEDcar = cars[userCARseats]
+//Finds recommended car
+}
+
+function tellUSERrecommendedCAR() {
+    alert("The recommended car that meets your requirements is " + cars[userCARseats] + ", it has " + carSEATS[userCARseats] + " seats");
+//Tells user recommended car and its number of seats
+}
+
+function checkVARIABLES() {
+    console.log(userNAME);
+    console.log(userAGE);
+    console.log(userLICENCE);
+    console.log(userCARseats);
+    console.log(userRECOMMENDEDcar);
+//Easy to check if info is right
+}
