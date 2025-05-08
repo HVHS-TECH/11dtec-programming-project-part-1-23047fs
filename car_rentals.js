96ec3350e8c05c6fb2be49945ef820590fd31b6d
23047fs <23047fs@hvhs.school.nz>
2025-05-08 01:07:39 +0000

    May 8, 2025, 1:07 AM

--- car_rentals.js
+++ car_rentals.js
@@ -64,7 +64,10 @@ function userLICENCEcheck() {
 function findUSERcar() {
     userCARvariables = prompt(userNAME + " what would be the ideal number of seats in your vehicle, 1-8");
     userCARvariables = userCARvariables - 1;
-    if (userCARvariables > 3) {
+    if (userCARvariables >= 4) {
+        userCARvariables = userCARvariables - 1;
+    }
+    else if (userCARvariables >= 7) {
         userCARvariables = userCARvariables - 2;
     }
 
